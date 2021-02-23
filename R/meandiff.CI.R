1274cf004090678dbf92863109251a7b89f1d3e8
Marie Delacre <marie.delacre@ulb.ac.be>
2021-02-23 13:50:40 +0100

    meandiff.CI function added

--- R/meandiff.CI.R
+++ R/meandiff.CI.R
@@ -1,15 +1,18 @@
 #' Function to compute CI around the raw mean difference
 #'
-#' @param Group.1 a (non-empty) numeric vector of data values.
-#' @param Group.2 a (non-empty) numeric vector of data values.
+#' @param m1 the average score of the first group
+#' @param m2 the average score of the second group
+#' @param sd1 the standard deviation the first group
+#' @param sd2 the standard deviation the second group
+#' @param n1 the first sample size
+#' @param n2 the second sample size
 #' @param conf.level confidence level of the interval
 #' @param var.equal a logical variable indicating whether to assume equality of population variances.
 #' If TRUE the pooled variance is used to estimate the standard error. Otherwise, the standard error is estimated based on
 #' unpooled variance.
 #' @param alternative a character string specifying the alternative hypothesis, must be one of "two.sided" (default), "greater" or "less".
-#' @param na.rm set whether Missing Values should be excluded (na.rm = TRUE) or not (na.rm = FALSE) - defaults to TRUE.
 #'
-#' @export datameandiff.CI
+#' @export meandiff.CI
 #' @S3method outliers_mcd default
 #' @S3method print outliers_mcd
 #'
@@ -17,32 +20,36 @@
 #' @return Returns raw mean difference, (1-alpha)% confidence interval around mean difference, standard error
 #' @importFrom stats na.omit sd pt uniroot
 
-datameandiff.CI <- function(Group.1,Group.2,conf.level,var.equal,alternative,na.rm) UseMethod("datameandiff.CI")
+meandiff.CI <- function(m1,m2,sd1,sd2,n1,n2,conf.level,var.equal,alternative) UseMethod("meandiff.CI")
 
-datameandiff.CIEst <- function(Group.1,
-                            Group.2,
-                            conf.level=.95,
+meandiff.CIEst <- function(m1,m2,sd1,sd2,
+                            n1,n2,conf.level=.95,
                             var.equal=FALSE,
-                            alternative="two.sided",
-                            na.rm=TRUE){
+                           alternative="two.sided"){
 
-  if (na.rm == TRUE ) {
-    Group.1 <- na.omit(Group.1)
-    Group.2 <- na.omit(Group.2)
-  } else {
-    Group.1 <- Group.1
-    Group.2 <- Group.2
-  }
+#Alert message if at least one of the parameter is neither numeric nor integer
 
-    if(inherits(Group.1,c("numeric","integer")) == FALSE |inherits(Group.2,c("numeric","integer")) == FALSE)
-      stop("Data are neither numeric nor integer")
+param <- data.frame(m1,m2,sd1,sd2,n1,n2)
+vect <- NULL
+for (i in seq_len(length(param))){
+  if(inherits(param[,i],c("numeric","integer"))==FALSE){
+    vect <-   c(vect,names(param[i]))
+  } else {vect=vect}
+}
 
-  n1 <- length(Group.1)
-  n2 <- length(Group.2)
-  m1 <- mean(Group.1)
-  m2 <- mean(Group.2)
-  sd1 <- sd(Group.1)
-  sd2 <- sd(Group.2)
+    if(inherits(c(m1,m2,sd1,sd2,n1,n2),c("numeric","integer"))==FALSE){
+        if(length(vect)==1){
+            obj <- vect
+            alert="is neither numeric nor integer"
+        } else if (length(vect)>1){
+            obj <-paste(paste(vect[-length(vect)],collapse=", "),"and",vect[length(vect)])
+            alert="are neither numeric nor integer"
+        }
+
+      stop(paste(obj,alert))
+
+    }
+
 
   if(alternative=="two.sided"){
 
@@ -186,27 +193,25 @@ datameandiff.CIEst <- function(Group.1,
 
 }
 
-# Adding a default method in defining a function called datameandiff.CI.default
-datameandiff.CI.default <- function(
-           Group.1,
-           Group.2,
-           conf.level=.95,
-           var.equal=FALSE,
-           alternative="two.sided",
-           na.rm=TRUE){
+# Adding a default method in defining a function called meandiff.CI.default
 
-  out <- datameandiff.CIEst(Group.1,Group.2,conf.level,var.equal,alternative,na.rm)
+meandiff.CI.default <- function(m1,m2,sd1,sd2,
+                           n1,n2,conf.level=.95,
+                           var.equal=FALSE,
+                           alternative="two.sided"){
+
+  out <- meandiff.CIEst(m1,m2,sd1,sd2,n1,n2,conf.level,var.equal,alternative)
   out$Meandiff <- out$Meandiff
   out$std.error <- out$std.error
   out$call <- match.call()
   out$CI <- out$CI
   out$conf.level <- out$conf.level
 
-  class(out) <- "datameandiff.CI"
+  class(out) <- "meandiff.CI"
   out
 }
 
-print.datameandiff.CI <- function(x,...){
+print.meandiff.CI <- function(x,...){
   cat("Call:\n")
   print(x$call)
 
