1506560a9310cfe3990ae926c1a76c79f1106cc9
Marie Delacre <marie.delacre@ulb.ac.be>
2021-03-18 14:48:04 +0100

    exportMethod

--- R/cohen.CI.R
+++ R/cohen.CI.R
@@ -15,8 +15,7 @@
 #' @param alternative a character string specifying the alternative hypothesis, must be one of "two.sided" (default), "greater" or "less".
 #'
 #' @export cohen.CI
-#' @exportMethod cohen.CI default
-#' @exportMethod print cohen.CI
+#' @exportMethod cohen.CI
 #'
 #' @keywords Cohen's effect sizes, confidence interval
 #' @return Returns Cohen's estimators of effect size and (1-alpha)% confidence interval around it, standard error
